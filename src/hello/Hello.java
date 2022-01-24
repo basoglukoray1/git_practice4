0629a59ee917a331f65294549961b6e89d94f1e3
Koray Basoglu <basoglukoray1@gmail.com>
2022-01-24 10:39:17 -0500

    Step 2 start

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("B25 learning git and github");
+
 
     }
 }
